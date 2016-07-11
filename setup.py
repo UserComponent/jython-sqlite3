3f985793adca895d646ec6bf8a24ad5090407c87
Anthony Hendrickson <ahendrickson@ausinc.com>
2016-07-11 02:42:42 -0400

    updated package info

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup (
     name = "jython-sqlite3",
-    version = "0.1.1",
+    version = "0.1.2",
     py_modules = ["sqlite3"],
     author = "Anthony Hendrickson",
     author_email = "ahendrickson@ausinc.com",
-    url="https://github.com/anthonyhendrickson/jython-sqlite3"
+    url = "https://github.com/anthonyhendrickson/jython-sqlite3",
+    download_url = "https://github.com/anthonyhendrickson/jython-sqlite3/archive/0.1.2.tar.gz"
 )
