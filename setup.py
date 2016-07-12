c574043f713b71ea72d4b5bdbc70e87ee2973da5
Anthony Hendrickson <ahendrickson@ausinc.com>
2016-07-12 12:43:34 -0400

    updated setup config to use markdown file on PyPi

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup (
     name = "jython-sqlite3",
-    version = "0.1.3.1",
+    version = "0.1.3.2",
     description = "Lightweight wrapper for using sqlite3 in Jython (JVM alternative to C Python's sqlite3 extension module - 'pysqlite')",
     packages = ["sqlite3"],
     author = "Anthony Hendrickson",
     author_email = "ahendrickson@ausinc.com",
     url = "https://github.com/anthonyhendrickson/jython-sqlite3",
-    download_url = "https://github.com/anthonyhendrickson/jython-sqlite3/archive/0.1.3.1.tar.gz"
+    download_url = "https://github.com/anthonyhendrickson/jython-sqlite3/archive/0.1.3.2.tar.gz"
 )
